7460368e152af02b61b10f77441d19660990af2f
nielzinnxd <danieL_rocha2009@hotmail.com>
2016-12-26 18:03:52 -0200

    teste commit

--- app/src/main/java/teamtreehouse/com/iamhere/Pesquisa.java
+++ app/src/main/java/teamtreehouse/com/iamhere/Pesquisa.java
@@ -3,9 +3,22 @@ package teamtreehouse.com.iamhere;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 /**
@@ -26,7 +39,15 @@ public class Pesquisa extends Activity {
         pesquisar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              startActivity(new Intent(Pesquisa.this,GPSMainActivity.class));
+              //startActivity(new Intent(Pesquisa.this,GPSMainActivity.class));
+
+
+
+
+
+
+
+
 
             }
         });

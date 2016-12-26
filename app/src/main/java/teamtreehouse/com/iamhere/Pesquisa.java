1ffdb1022f529d05ea0f15548462174d524d58ea
nielzinnxd <daniel_rocha2009@hotmail.com>
2016-12-26 11:10:24 -0200

    Update Pesquisa.java

--- app/src/main/java/teamtreehouse/com/iamhere/Pesquisa.java
+++ app/src/main/java/teamtreehouse/com/iamhere/Pesquisa.java
@@ -19,7 +19,7 @@ public class Pesquisa extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pesquisa);
+        setContentView(R.layout.descricao);
 
         pesquisar = (Button) findViewById(R.id.btn_pesquisar);
         cep = (EditText) findViewById(R.id.cpf);
